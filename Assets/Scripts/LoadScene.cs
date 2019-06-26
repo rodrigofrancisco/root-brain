0c9d07380a94445dc12555e2e75c46b81546d62f
rhofp <rhodfra@gmail.com>
2019-06-25 21:22:07 -0500

    Agrego modificacion de Daniel

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    public int sceneIndex;
+
+    void OnMouseDown() {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void CargarEscena() {
+        SceneManager.LoadScene(sceneIndex);
+        Debug.Log("asdf");
+    }
+}
