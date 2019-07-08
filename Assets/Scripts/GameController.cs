ce65a5a95e8ff260a331171139ad8d5fd01f1624
rhofp <rhodfra@gmail.com>
2019-07-07 22:02:37 -0500

    corrijo boton close y main scene

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,48 +11,50 @@ public class GameController : MonoBehaviour{
     public GameObject gotaprefab;
     public GameObject gameOverPanel;
     public GameObject onStartGamePanel;
-    //private float timer = 0f;
-    //private int timer2 = 0;
-    //private int temp = 0;
-    //private int questionCouter = 0;
+
+    public bool started = false;
+    private float timer = 0f;
+    private int timer2 = 0;
+    private int temp = 0;
+    private int questionCounter = 0;
     
 
     private List<GameObject> _instances = new List<GameObject>();
 
     void Start(){
-        StartCoroutine(DisplayNextQuestion());
+        //StartCoroutine(DisplayNextQuestion());          
     }
 
-/*     void Update(){
+     void Update(){
         timer += Time.deltaTime;
         timer2 = (int) timer;
 
-        if (timer2 != 0 && timer2 %3 == 0 && timer2 != temp){
-            temp = timer2;
-            if (questionCouter < dataController.questions.Length){
-                CreateDropsAndQuestion(questionCouter);
-                questionCouter ++;
-            }
-        }
-    } */
-
-
-    IEnumerator DisplayNextQuestion(){
-        //Tantas preguntas como grupos de 3 gotas
-        for (int i = 0 ; i < dataController.questions.Length; i++){ 
-            yield return new WaitForSeconds(3);
+        if (started){
             onStartGamePanel.SetActive(false);
-            bool p = GameObject.Find("Personaje").GetComponent<PersonajeLife>().isAlive;
-            if(p) CreateDropsAndQuestion(i);
-            else {
-                Debug.Log("ya se murio");
-                for(int j = 0; j< _instances.Count; j++){
-                    Destroy(_instances[j]);
-                }
+            if (timer2 != 0 && timer2 %3 == 0 && timer2 != temp){
+                temp = timer2;
+                if (questionCounter < dataController.questions.Length){
+                    bool p = GameObject.Find("Personaje").GetComponent<PersonajeLife>().isAlive;
+                    if(p) CreateDropsAndQuestion(questionCounter);
+                    else {
+                        Debug.Log("ya se murio");
+                        for(int j = 0; j< _instances.Count; j++){
+                            Destroy(_instances[j]);
+                        }
+                        gameOverPanel.SetActive(true);
+                    }
+                    questionCounter ++;
+                }   
+            }
+            if (GameObject.Find("Personaje").GetComponent<PersonajeLife>().gameOver){
                 gameOverPanel.SetActive(true);
             }
         }
-    } 
+    }
+
+    public void StartGame(){
+        started = true;
+    }
 
     void CreateDropsAndQuestion(int index){
         
---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info : MonoBehaviour{
+    public bool onOffPanel = false;
+    public GameObject infoPanel;
+    public void ToglePanelInfo(){
+        if (onOffPanel){
+            onOffPanel = false;
+            infoPanel.SetActive(false);
+        }
+        else {
+            onOffPanel = true;
+            infoPanel.SetActive(true);
+        }    
+    }
+}
